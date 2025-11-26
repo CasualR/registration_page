0485337167516eed2f4640a1d20a6f3d8fed69c9
CasualR <kolyabaner@gmail.com>
2025-11-26 21:11:07 +0100

    html template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 
 function App() {
 
   return (
     <>
-      
+      <a href="" className="logo">
+        <img src="" alt="" />
+        <span> Finnger </span>
+      </a>
+      <div className='text'>
+        <h1> Holla, Welcome Back </h1>
+        <h2> Hey, welcome back to your special place </h2>
+      </div>
+      <form className='signin__form' action="">
+        <input type="text" id="username" placeholder='Enter username' name='username' required />
+        <input type="password" placeholder='Enter password' id="password" name="password" required />
+        <div>
+          <label>
+            <input type="checkbox" checked="checked" name='remember' />
+            Remember me
+          </label>
+          <a href="#" className='signin__forgot'> Forgot password? </a>
+        </div>
+        <button className='signin__button' type='submit'> Sign In </button>
+      </form>
+      <div> Don't have an account? <a href="#"> Sign Up </a> </div>
     </>
   )
 }
