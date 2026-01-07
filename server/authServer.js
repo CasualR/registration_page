4f4436c0c03b3a2545cdc559476cd57b1b1a0633
CasualR <kolyabaner@gmail.com>
2026-01-07 23:34:16 +0100

    Refresh Token

--- server/authServer.js
+++ server/authServer.js
@@ -18,16 +18,24 @@ app.get('/api/user', (req, res) => {
   res.send(users)
 })
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token;
+})
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
+app.post('/login', (req, res) => {
+  // Authenticate User
+
+  const username = req.body.username;
+  const user = { name: username }
+
+  const accessToken = generateAccessToken(user)
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  res.json({ accessToken: accessToken })
+
+})
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' })
 }
 
 const port = process.env.PORT || 3000
