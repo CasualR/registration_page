fd68fd72df241ba5bb9f85923a6bce0ddd3c7cb2
CasualR <kolyabaner@gmail.com>
2026-01-10 23:53:56 +0100

    change

--- server/authServer.js
+++ server/authServer.js
@@ -7,7 +7,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 const app = express()
 
-app.use(cors({ origin: true, credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -18,8 +18,17 @@ app.get('/api/user', (req, res) => {
   res.send(users)
 })
 
+let refreshTokens = []
+
 app.post('/token', (req, res) => {
   const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401)
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken })
+  })
 })
 
 app.post('/login', (req, res) => {
@@ -30,15 +39,21 @@ app.post('/login', (req, res) => {
 
   const accessToken = generateAccessToken(user)
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-  res.json({ accessToken: accessToken })
+  refreshTokens.push(refreshToken)
+  res.json({ accessToken: accessToken, refreshToken: refreshToken })
 
+})
+
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
 })
 
 function generateAccessToken(user) {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' })
 }
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`)
